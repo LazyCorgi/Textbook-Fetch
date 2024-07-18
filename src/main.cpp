f850c72d91f406ef490ca90740584f5fbb5f55d8
Ke <2798595008@qq.com>
2024-07-18 14:11:44 +0800

    美化了进度条输出

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
 #include "downloader.h"
 
+#include <iomanip>
 #include <iostream>
+#include <sstream>
+
+void showProgressBar(double percentage) {
+  const int barWidth = 50;
+  std::cout << "\033[32m[";
+  int pos = barWidth * percentage;
+  for (int i = 0; i < barWidth; ++i) {
+    if (i < pos)
+      std::cout << "+"; // green block
+    else
+      std::cout << " ";
+  }
+  std::cout << "] \033[0m" << std::fixed << std::setprecision(2)
+            << (percentage * 100.0) << "% ";
+  std::cout.flush();
+}
 
 int main() {
   std::string i_text, o_text;
   i_text = "https://basic.smartedu.cn/tchMaterial/"
            "detail?contentType=assets_document&contentId=c71014f5-3bf1-47e2-"
            "b9db-d70b79f2b33d&catalogType=tchMaterial&subCatalog=tchMaterial";
-  // https
-  //     : //
-  //     r3-ndr-private.ykt.cbern.com.cn/edu_product/esp/assets/c71014f5-3bf1-47e2-b9db-d70b79f2b33d.pkg/pdf.pdf
-  // https
-  //     : //
-  //     r3-ndr-private.ykt.cbern.com.cn/edu_product/esp/assets/7786158e-01d7-4544-a358-5759664c78ea.pkg/pdf.pdf
   std::string::size_type pos1 = i_text.find("contentId="),
                          pos2 = i_text.find("&catalogType=");
   if (pos1 != std::string::npos) {
@@ -25,15 +36,20 @@ int main() {
                     o_text + ".pkg/pdf.pdf";
   std::cout << url << std::endl;
 
+  std::string downloadPath = "output.pdf";
+
   Downloader downloader;
-  downloader.download(
-      url, "../download/abc.pdf", [](double total, double downloaded) {
-        if (total > 0) {
-          std::cout << "Total: " << total << " bytes\t";
-          std::cout << "Progress: " << (downloaded / total) * 100 << "%\r";
-          std::cout.flush();
-        }
-      });
+  downloader.download(url, downloadPath, [](double total, double downloaded) {
+    if (total > 0) {
+      static double predownloaded = 0;
+      showProgressBar(downloaded / total);
+      printf("\033[0m(总计%.2f MB, %.2f KB/s)   \r", total / 1024.0 / 1024.0,
+             (downloaded - predownloaded) / 1024.0);
+      predownloaded = downloaded;
+    }
+  });
+
+  std::cout << std::endl << "下载成功" << std::endl;
 
   return 0;
 }
